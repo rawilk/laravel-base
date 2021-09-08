5513b3a1ba624baa41f513883714dcb1bfdf59fe
Randall Wilk <wilk.randall@gmail.com>
2021-09-08 15:09:49 -0500

    wip

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -35,7 +35,9 @@ return (new PhpCsFixer\Config)
             'on_multiline' => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
         ],
-        'space' => 'none',
+        'types_spaces' => [
+            'space' => 'none',
+        ],
         'single_trait_insert_per_statement' => false,
     ])
     ->setFinder($finder);
