732fcd9da5ef43e9c835e8540f278bbd0b2c4955
Randall Wilk <wilk.randall@gmail.com>
2021-09-15 09:42:53 -0500

    x-cloak to prevent flashing on page load

--- resources/views/components/button/scroll-to-top-button.blade.php
+++ resources/views/components/button/scroll-to-top-button.blade.php
@@ -1,4 +1,4 @@
-<div x-data="scrollToTopButton" class="scroll-to-top-button | absolute top-0 right-0 h-screen w-screen pointer-events-none">
+<div x-cloak x-data="scrollToTopButton" class="scroll-to-top-button | absolute top-0 right-0 h-screen w-screen pointer-events-none">
     <button
         x-on:click="toTop"
         type="button"
