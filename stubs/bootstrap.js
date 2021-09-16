48a775783a2761a3b75581facff3f70d1d2729aa
Randall Wilk <wilk.randall@gmail.com>
2021-09-16 09:52:50 -0500

    Remove uneeded magic

--- stubs/bootstrap.js
+++ stubs/bootstrap.js
@@ -2,9 +2,7 @@ import Alpine from 'alpinejs';
 import flatpickr from 'flatpickr';
 import Clipboard from '@ryangjchandler/alpine-clipboard';
 import { createPopper } from '@popperjs/core';
-import $root from '../../vendor/rawilk/laravel-base/resources/js/magics/$root';
 
-Alpine.magic('root', $root);
 Alpine.plugin(Clipboard);
 
 window.Alpine = Alpine;
