690b4f50dea90060f144aa812b9ad6c13c57a3cb
Randall Wilk <wilk.randall@gmail.com>
2021-09-21 11:32:33 -0500

    Refactor stubs into folders

--- src/LaravelBaseServiceProvider.php
+++ src/LaravelBaseServiceProvider.php
@@ -92,12 +92,12 @@ class LaravelBaseServiceProvider extends ServiceProvider
         ], 'laravel-base-config');
 
         $this->publishes([
-            __DIR__ . '/../stubs/LaravelBaseServiceProvider.php' => app_path('Providers/LaravelBaseServiceProvider.php'),
-            __DIR__ . '/../stubs/tailwind-safelist-preset.js' => base_path('tailwind-safelist-preset.js'),
-            __DIR__ . '/../stubs/tailwind-preset.js' => base_path('tailwind-preset.js'),
-            __DIR__ . '/../stubs/app.css' => resource_path('css/app.css'),
-            __DIR__ . '/../stubs/bootstrap.js' => resource_path('js/bootstrap.js'),
-            __DIR__ . '/../stubs/webpack.mix.js' => base_path('webpack.mix.js'),
+            __DIR__ . '/../stubs/app/Providers/LaravelBaseServiceProvider.php' => app_path('Providers/LaravelBaseServiceProvider.php'),
+            __DIR__ . '/../stubs/resources/css/app.css' => resource_path('css/app.css'),
+            __DIR__ . '/../stubs/resources/js/bootstrap.js' => resource_path('js/bootstrap.js'),
+            __DIR__ . '/../stubs/build/tailwind-safelist-preset.js' => base_path('tailwind-safelist-preset.js'),
+            __DIR__ . '/../stubs/build/tailwind-preset.js' => base_path('tailwind-preset.js'),
+            __DIR__ . '/../stubs/build/webpack.mix.js' => base_path('webpack.mix.js'),
         ], 'laravel-base-support');
 
         $this->publishes([
