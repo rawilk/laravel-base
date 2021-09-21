efa6e6b0e8b732351149152933b6a3c511d8a504
Randall Wilk <wilk.randall@gmail.com>
2021-09-21 11:24:23 -0500

    Set default password validation rules in provider stub

--- stubs/LaravelBaseServiceProvider.php
+++ stubs/LaravelBaseServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 use Rawilk\LaravelBase\LaravelBase;
 
 final class LaravelBaseServiceProvider extends ServiceProvider
@@ -14,5 +15,17 @@ final class LaravelBaseServiceProvider extends ServiceProvider
         // LaravelBase::findAppTimezoneUsing(function () {
         //     return config('app.timezone');
         // });
+
+        $this->setDefaultPasswordRules();
+    }
+
+    private function setDefaultPasswordRules(): void
+    {
+        Password::defaults(function () {
+            return Password::min(6)
+                ->when($this->app->isProduction(), function (Password $rule) {
+                    return $rule->uncompromised();
+                });
+        });
     }
 }
