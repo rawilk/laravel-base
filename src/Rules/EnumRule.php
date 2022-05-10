86d43d3b554fdf3ca749549e7f8e6870de4faa96
Randall Wilk <wilk.randall@gmail.com>
2022-05-10 11:28:18 -0500

    Allow validating for specific cases in an enum

--- src/Rules/EnumRule.php
+++ src/Rules/EnumRule.php
@@ -4,19 +4,30 @@ namespace Rawilk\LaravelBase\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Support\Facades\Lang;
-use Rawilk\LaravelBase\Contracts\Enums\HasLabel;
 
 class EnumRule implements Rule
 {
     protected $value;
+    protected ?array $onlyCases = null;
 
     public function __construct(protected readonly string $enum)
     {
     }
 
+    public function onlyCases(array $cases): self
+    {
+        $this->onlyCases = $cases;
+
+        return $this;
+    }
+
     public function passes($attribute, $value): bool
     {
         $this->value = $value;
+
+        if ($this->onlyCases) {
+            return $this->passesForCases($value);
+        }
 
         if ($value instanceof $this->enum) {
             return true;
@@ -32,5 +43,16 @@ class EnumRule implements Rule
             'enum' => $this->enum,
             'other' => implode(', ', enumToLabels($this->enum)),
         ]);
+    }
+
+    private function passesForCases($value): bool
+    {
+        $case = $this->enum::tryFrom($value);
+
+        if (! $case) {
+            return false;
+        }
+
+        return in_array($case, $this->onlyCases, true);
     }
 }
