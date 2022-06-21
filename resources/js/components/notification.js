a3cbc672a24a0dcb3e176e3aceba6b621876cfa8
Randall Wilk <wilk.randall@gmail.com>
2022-06-21 08:48:15 -0500

    Add support for browser notifications

--- resources/js/components/notification.js
+++ resources/js/components/notification.js
@@ -1,7 +1,23 @@
+// TODO: Utilize a service worker for push notifications
+const pushNotificationSupported = () => 'serviceWorker' in navigator && 'PushManager' in window;
+
 export default options => ({
     notices: [],
     visible: [],
     timeShown: options.timeout || 5000, // in ms
+    onclick: options.onclick || null,
+    callbacks: {},
+    onInit: options.onInit || null,
+
+    init() {
+        typeof this.onInit === 'function' && this.onInit();
+    },
+
+    registerCallback(name, callback) {
+        this.callbacks[name] = callback;
+
+        return this;
+    },
 
     add(notice) {
         this.notices.push(notice);
@@ -15,13 +31,72 @@ export default options => ({
             return;
         }
 
-        this.visible.push(notification);
+        // Attempt to display a browser notification if the user has granted
+        // us permission to...
+        if (notification.defaultToBrowser) {
+            this.attemptBrowserNotification(notification);
+        } else {
+            this.visible.push(notification);
+        }
 
         if (notification.autoDismiss) {
             setTimeout(() => {
                 this.remove(id);
             }, this.timeShown);
         }
+    },
+
+    attemptBrowserNotification(notification) {
+        const permission = Notification.permission;
+
+        switch (permission) {
+            case 'granted':
+                this.showBrowserNotification(notification);
+                break;
+            case 'default':
+                if (pushNotificationSupported()) {
+                    Notification.requestPermission().then(permission => {
+                        if (permission === 'granted') {
+                            this.showBrowserNotification(notification);
+                        }
+                    });
+                } else {
+                    this.visible.push(notification);
+                }
+                break;
+            default:
+                this.visible.push(notification);
+                break;
+        }
+    },
+
+    showBrowserNotification(notification) {
+        const requireInteraction = notification.autoDismiss ? false : true;
+
+        const browserNotification = new Notification(notification.title || 'Notification', {
+            body: notification.text || notification.body || '',
+            data: { ...notification },
+            requireInteraction,
+        });
+
+        browserNotification.onclose = () => {
+            try {
+                this.remove(browserNotification.data.id);
+            } catch (error) {}
+        };
+
+        browserNotification.onclick = () => {
+            browserNotification.close();
+            window.parent.focus();
+
+            try {
+                this.remove(browserNotification.data.id);
+            } catch (error) {}
+
+            if (this.callbacks.onclick) {
+                this.callbacks.onclick(browserNotification);
+            }
+        };
     },
 
     remove(id) {
