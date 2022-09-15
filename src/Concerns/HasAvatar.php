3b10af428b6b39905586ece3557d8294c1cc4f74
Randall Wilk <wilk.randall@gmail.com>
2022-09-15 15:10:12 -0500

    Add support for external avatar urls

--- src/Concerns/HasAvatar.php
+++ src/Concerns/HasAvatar.php
@@ -5,6 +5,7 @@ namespace Rawilk\LaravelBase\Concerns;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Rawilk\LaravelBase\Features;
 
 /**
@@ -42,13 +43,20 @@ trait HasAvatar
     public function getAvatarUrlAttribute(): string
     {
         return $this->avatar_path
-            ? Storage::disk($this->avatarDisk())->url($this->avatar_path)
+            ? $this->getStoredAvatarUrl()
             : $this->defaultAvatarUrl();
     }
 
     protected function defaultAvatarUrl(): string
     {
         return 'https://ui-avatars.com/api/?name=' . urlencode($this->name->full) . '&color=7F9CF5&background=EBF4FF';
+    }
+
+    protected function getStoredAvatarUrl(): string
+    {
+        return Str::startsWith(Str::lower($this->avatar_path), 'http')
+            ? $this->avatar_path
+            : Storage::disk($this->avatarDisk())->url($this->avatar_path);
     }
 
     protected function avatarDisk(): string
